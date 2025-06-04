7a984772206046cdfa56692616ad08012b873f09
oooo12-git <jhdmbwy12@gmail.com>
2025-06-04 09:38:10 +0900

    [더미 데이터 추가, 주석 수정]

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -20,36 +20,26 @@ export async function BlogPosts({
         </div>
       </div>
       <div className="flex flex-col space-y-1">
-        {upperPosts.posts
-          // .sort((a, b) => {
-          //   if (
-          //     new Date(a.metadata.publishedAt) >
-          //     new Date(b.metadata.publishedAt) // Date가 클수록 최신
-          //   ) {
-          //     return -1; // -1을 반환하면 a가 b보다 앞에 위치
-          //   }
-          //   return 1; // 1을 반환하면 b가 a보다 앞에 위치
-          // })
-          .map((post) => (
-            <Link
-              key={post.slug} // React에서 key prop이 필요한 이유 : key를 통해 React는 어떤 항목이 변경, 추가 또는 제거되었는지 빠르게 식별할 수 있습니다. key가 없으면 React는 모든 항목을 다시 렌더링해야 할 수 있어 성능이 저하됩니다.
-              className="flex flex-col"
-              // space-y-1은 요소 사이에 1px의 간격을 줍니다.
-              // mb-4는 요소 아래에 4px의 간격을 줍니다.
-              href={`/blog/${post.slug}`}
-            >
-              <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 items-center justify-between">
-                {/* tabular-nums :  모든 숫자가 동일한 너비를 가지도록 합니다.  */}
-                <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums font-thin text-xs text-center">
-                  {post.metadata.publishedAt}
-                </p>
-                {/* truncate: 제목이 컨테이너의 너비를 초과할 경우 자동으로 말줄임표(...) 표시 */}
-                <p className="w-[550px] text-neutral-900 dark:text-neutral-100 tracking-tight truncate font-extralight text-sm">
-                  {post.metadata.title}
-                </p>
-              </div>
-            </Link>
-          ))}
+        {upperPosts.posts.map((post) => (
+          <Link
+            key={post.slug} // React에서 key prop이 필요한 이유 : key를 통해 React는 어떤 항목이 변경, 추가 또는 제거되었는지 빠르게 식별할 수 있습니다. key가 없으면 React는 모든 항목을 다시 렌더링해야 할 수 있어 성능이 저하됩니다.
+            className="flex flex-col"
+            // space-y-1은 요소 사이에 1px의 간격을 줍니다.
+            // mb-4는 요소 아래에 4px의 간격을 줍니다.
+            href={`/blog/${post.slug}`}
+          >
+            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 items-center justify-between">
+              {/* tabular-nums :  모든 숫자가 동일한 너비를 가지도록 합니다.  */}
+              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums font-thin text-xs text-center">
+                {post.metadata.publishedAt}
+              </p>
+              {/* truncate: 제목이 컨테이너의 너비를 초과할 경우 자동으로 말줄임표(...) 표시 */}
+              <p className="w-[550px] text-neutral-900 dark:text-neutral-100 tracking-tight truncate font-extralight text-sm">
+                {post.metadata.title}
+              </p>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
--- app/components/PostsBottom.tsx
+++ app/components/PostsBottom.tsx
@@ -8,44 +8,31 @@ export async function BlogPostsBottom({
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
-  const lowerPosts = await getPosts(locale, 3, 4);
+  const lowerPosts = await getPosts(locale, 5, 4);
 
   return (
     <div className="border-b-1 border-t-1 pt-2 pb-2">
       <div className="flex flex-col space-y-1">
-        {lowerPosts.posts
-          // .sort((a, b) => {
-          //   if (
-          //     new Date(a.metadata.publishedAt) >
-          //     new Date(b.metadata.publishedAt) // Date가 클수록 최신
-          //   ) {
-          //     return -1; // -1을 반환하면 a가 b보다 앞에 위치
-          //   }
-          //   return 1; // 1을 반환하면 b가 a보다 앞에 위치
-          // })
-          // .slice(3, 7) // 앞에서 3개의 포스트만 선택 slice(0, 3)으로 정렬된 배열에서:
-          // // 3: 3번째 요소부터 시작
-          // // 7: 7번째 요소 직전까지 선택 (즉, 3, 4, 5, 6 인덱스의 요소만 선택)
-          .map((post) => (
-            <Link
-              key={post.slug} // React에서 key prop이 필요한 이유 : key를 통해 React는 어떤 항목이 변경, 추가 또는 제거되었는지 빠르게 식별할 수 있습니다. key가 없으면 React는 모든 항목을 다시 렌더링해야 할 수 있어 성능이 저하됩니다.
-              className="flex flex-col"
-              // space-y-1은 요소 사이에 1px의 간격을 줍니다.
-              // mb-4는 요소 아래에 4px의 간격을 줍니다.
-              href={`/blog/${post.slug}`}
-            >
-              <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 items-center justify-between">
-                {/* tabular-nums :  모든 숫자가 동일한 너비를 가지도록 합니다.  */}
-                <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums font-thin text-xs text-center">
-                  {post.metadata.publishedAt}
-                </p>
-                {/* truncate: 제목이 컨테이너의 너비를 초과할 경우 자동으로 말줄임표(...) 표시 */}
-                <p className="w-[550px] text-neutral-900 dark:text-neutral-100 tracking-tight truncate font-extralight text-sm">
-                  {post.metadata.title}
-                </p>
-              </div>
-            </Link>
-          ))}
+        {lowerPosts.posts.map((post) => (
+          <Link
+            key={post.slug} // React에서 key prop이 필요한 이유 : key를 통해 React는 어떤 항목이 변경, 추가 또는 제거되었는지 빠르게 식별할 수 있습니다. key가 없으면 React는 모든 항목을 다시 렌더링해야 할 수 있어 성능이 저하됩니다.
+            className="flex flex-col"
+            // space-y-1은 요소 사이에 1px의 간격을 줍니다.
+            // mb-4는 요소 아래에 4px의 간격을 줍니다.
+            href={`/blog/${post.slug}`}
+          >
+            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 items-center justify-between">
+              {/* tabular-nums :  모든 숫자가 동일한 너비를 가지도록 합니다.  */}
+              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums font-thin text-xs text-center">
+                {post.metadata.publishedAt}
+              </p>
+              {/* truncate: 제목이 컨테이너의 너비를 초과할 경우 자동으로 말줄임표(...) 표시 */}
+              <p className="w-[550px] text-neutral-900 dark:text-neutral-100 tracking-tight truncate font-extralight text-sm">
+                {post.metadata.title}
+              </p>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
