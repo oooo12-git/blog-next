5373afeef39a5b517d3e5f272abc5a58840777fc
oooo12-git <jhdmbwy12@gmail.com>
2025-06-12 15:05:22 +0900

    [BLOG-34]: About 구성하기

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,28 +1,65 @@
 import { useTranslations } from "next-intl";
+import Image from "next/image";
+import profileLight from "./김재현_about_light.jpeg";
+import profileDark from "./김재현_about_dark.jpeg";
+import Link from "next/link";
 
 export default function AboutPage() {
   const t = useTranslations("about");
 
   return (
-    <div className="px-4 my-2 sm:px-4 dark:text-white">
-      <h2 className="text-2xl font-bold">{t("title")}</h2>
-      <ul className="my-2 ml-4 list-disc list-inside">
-        <li>
-          <p className="inline">{t("education")}</p>
-        </li>
-        <li>
-          <p className="inline">{t("experience")}</p>
-        </li>
-        <li>
-          <p className="inline">{t("training")}</p>
-        </li>
-        <li>
-          <p className="inline">{t("skills")}</p>
-        </li>
-        <li>
-          <p className="inline">{t("certificates")}</p>
-        </li>
-      </ul>
+    <div className="flex flex-col sm:flex-row justify-between px-4">
+      <div className="my-2 dark:text-white sm:w-[350px]">
+        <h2 className="mb-4 text-2xl font-bold">{t("title")}</h2>
+        <div className="text-sm rounded-lg bg-[#ECEAEA] dark:bg-gray-800 p-2 border border-black mb-4">
+          <h3 className="font-medium text-xl mb-2 text-black dark:text-white">
+            경력
+          </h3>
+          <ul className="font-normal list-disc list-outside pl-4 space-y-1">
+            <li>스튜디오 예약 시스템 개발 (2024.09 ~ 진행중)</li>
+            <li>
+              <Link
+                href="https://blog.naver.com/PostView.naver?blogId=n_cloudplatform&logNo=223790788385"
+                target="_blank"
+                className="text-gray-600 hover:underline hover:text-black dark:text-neutral-300 dark:hover:text-white"
+              >
+                RAG 활용 재무 보고서 챗봇 개발
+              </Link>
+              (2024.10 ~ 2025.01)
+            </li>
+            <li>광고 데이터 분석 (2019.09 ~ 2022.09)</li>
+          </ul>
+        </div>
+        <ul className="my-2 list-disc list-outside pl-4 font-normal">
+          <li>
+            <p className="inline">{t("education")}</p>
+          </li>
+          <li>
+            <p className="inline">{t("experience")}</p>
+          </li>
+          <li>
+            <p className="inline">{t("training")}</p>
+          </li>
+          <li>
+            <p className="inline">{t("skills")}</p>
+          </li>
+          <li>
+            <p className="inline">{t("certificates")}</p>
+          </li>
+        </ul>
+      </div>
+      <div className="w-[200px] sm:w-[300px]">
+        <Image
+          src={profileLight}
+          alt="profile"
+          className="mr-3 rounded-lg dark:hidden"
+        />
+        <Image
+          src={profileDark}
+          alt="profile"
+          className="mr-3 rounded-lg hidden dark:block"
+        />
+      </div>
     </div>
   );
 }
