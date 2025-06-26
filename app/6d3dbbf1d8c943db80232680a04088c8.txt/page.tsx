8c37a4ac1268f6553f47cba73f6bdef4c07723ca
oooo12-git <jhdmbwy12@gmail.com>
2025-06-26 13:33:28 +0900

    [BLOG-71]: indexnow api키 등록

---
+++ app/6d3dbbf1d8c943db80232680a04088c8.txt/page.tsx
@@ -0,0 +1,21 @@
+// import Image from "next/image";
+import { BlogPosts } from "@/app/components/Posts";
+import { Inter } from "next/font/google";
+import { FeaturedPost } from "@/app/components/Featured";
+import { BlogPostsBottom } from "@/app/components/PostsBottom";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default async function Home() {
+  // const t = useTranslations("HomePage");
+  // const { locale } = await params;
+  // const { posts } = await getPosts(locale, 4);
+
+  return (
+    <html>
+      <body>
+        <p>6d3dbbf1d8c943db80232680a04088c8</p>
+      </body>
+    </html>
+  );
+}
