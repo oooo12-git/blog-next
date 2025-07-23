62c96817cbc5949705b634342b216adc0073a42a
oooo12-git <jhdmbwy12@gmail.com>
2025-07-23 18:19:28 +0900

    robots.txt에 sitemap 주소 변경(정확하지 않은 주소로 인한 사이트맵 미인식 우려)

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://kimjaahyun.com/sitemap.xml",
+    sitemap: "https://www.kimjaahyun.com/sitemap.xml",
   };
 }
